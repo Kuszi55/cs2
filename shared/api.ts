c6a6346d68b4ef324723640daf73631fc3c6a808
Kuszi55 <marcel.siolek@gmail.com>
2025-10-29 16:50:53 +0100

    Add PlayerStats and DemoAnalysisResponse interfaces

--- shared/api.ts
+++ shared/api.ts
@@ -4,6 +4,24 @@
  * and/or small pure JS functions that can be used on both client and server
  */
 
+export interface PlayerStats {
+  name: string;
+  steam_id: string;
+  team: string;
+  kills: number;
+  deaths: number;
+  assists: number;
+}
+
+export interface DemoAnalysisResponse {
+  success: boolean;
+  map: string;
+  players: PlayerStats[];
+  score: { team_a: number; team_b: number };
+  rounds: number;
+  error?: string;
+}
+
 /**
  * Example response type for /api/demo
  */
